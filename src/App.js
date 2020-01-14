8583a4b34f1bfb310a36ceb3dfa2fe1d68a62844
devarcher <archershot@hotmail.com>
2020-01-13 20:54:37 -0600

    setup env w/ prettier and started fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+class App extends React.Component {
+  state = {
+    humans: []
+  };
 
+  componentDidMount() {
+    this.fetchHumans();
+  }
+
+  async fetchHumans() {
+    try {
+      const url = "https://randomuser.me/api?results=25";
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        hi
+      </div>
+    )
+  }
+}
 
 export default App;
